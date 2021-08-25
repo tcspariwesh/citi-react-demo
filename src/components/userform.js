e63c195b8c24f412ca9e3e6d0c0a552a1843a77e
tcspariwesh <tcspariwesh@gmail.com>
2021-08-25 16:48:37 +0530

    backend integration using Axios

--- src/components/userform.js
+++ src/components/userform.js
@@ -1,18 +1,38 @@
+import axios from "axios";
 import { useState } from "react";
 
 
 export function Userform() {
-    const [userform, setUserform] = useState({firstname:"Pariwesh"});
-    const handleEvent = function(event){
+    const [userform, setUserform] = useState({ firstname: "Pariwesh" });
+    const [users, setUsers] = useState([{ firstname: 'ram' }]);
+    function getAllUser(){
+        axios.get("http://localhost:3000/users")
+        .then(function(response){
+            console.log(response.data);
+            setUsers(response.data)
+        })
+    }
+    getAllUser();
+    const handleEvent = function (event) {
         console.log(event.target.value);
         setUserform({
-            firstname:event.target.value
+            firstname: event.target.value
         })
+    }
+    const saveUser = function () {
+        axios.post("http://localhost:3000/users", userform)
+            .then(function (response) {
+                console.log(response.data);
+            });
+        console.log('clicked...');
     }
     return (//JSX
         <div>userform
             <input value={userform.firstname} onChange={handleEvent}></input>
-
+            <button onClick={saveUser}> Save</button>
+            {users.map(function (user, index) {
+                return <div>{user.firstname}</div>
+            })}
         </div>
     );
 }
