0ecd5995837dc064e1fab3dabe848794346bd063
tcspariwesh <tcspariwesh@gmail.com>
2021-08-25 17:25:34 +0530

    handling multiple inputs

--- src/components/userform.js
+++ src/components/userform.js
@@ -5,30 +5,36 @@ import { useState } from "react";
 export function Userform() {
     const [userform, setUserform] = useState({ firstname: "Pariwesh" });
     const [users, setUsers] = useState([{ firstname: 'ram' }]);
-    function getAllUser(){
-        axios.get("http://localhost:3000/users")
-        .then(function(response){
-            console.log(response.data);
-            setUsers(response.data)
-        })
+    function getAllUser() {
+        if (!users.length)
+            axios.get("http://localhost:3000/users")
+                .then(function (response) {
+                    console.log(response.data);
+                    setUsers(response.data)
+                })
     }
     getAllUser();
     const handleEvent = function (event) {
-        console.log(event.target.value);
+    console.log(event.target.value);
         setUserform({
-            firstname: event.target.value
+           ...userform,  [event.target.name]: event.target.value //spread operator
         })
     }
     const saveUser = function () {
         axios.post("http://localhost:3000/users", userform)
             .then(function (response) {
                 console.log(response.data);
+            })
+            .catch(function (error) {
+                alert(error);
             });
         console.log('clicked...');
     }
     return (//JSX
         <div>userform
-            <input value={userform.firstname} onChange={handleEvent}></input>
+            <input value={userform.firstname} name='firstname' onChange={handleEvent}></input>
+            <input value={userform.age}  name='age' onChange={handleEvent}></input>
+
             <button onClick={saveUser}> Save</button>
             {users.map(function (user, index) {
                 return <div>{user.firstname}</div>
